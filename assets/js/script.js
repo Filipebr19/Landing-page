4d7bbc795844cb00919bc2da404f0e28807f2980
Filipebr19 <95660776+Filipebr19@users.noreply.github.com>
2023-02-20 15:41:36 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+function openMenu() {
+    document.getElementById('navBar').style.width = '40%';
+    document.querySelector('.container.header .background-menu').style.width = '100%';
+}
+
+function closeMenu() {
+    document.getElementById('navBar').style.width = '0';
+    document.querySelector('.container.header .background-menu').style.width = '0';
+}
+
+
+
+
+
+
